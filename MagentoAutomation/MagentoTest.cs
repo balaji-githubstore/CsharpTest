db1e22402ef989bb5c877cd339e5c3f2b93deec3
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2018-05-22 23:26:48 +0530

    Add project files.

---
+++ MagentoAutomation/MagentoTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading;
+using MagentoAutomation.Pageobjects;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace MagentoAutomation
+{
+    public class MagentoTest
+    {
+        [Test]
+        public void ValidCredential()
+        {
+            IWebDriver driver=null;
+            try
+            {
+                driver = new ChromeDriver(@"D:\Mine\Company\Maveric\Driver");
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+                driver.Url = "https://magento.com/";
+
+                //HomePage
+                HomePage homePage = new HomePage(driver);
+                homePage.ClickOnMyAccount();
+
+                //LoginPage
+                LoginPage loginPage = new LoginPage(driver);
+                loginPage.SetUserName("balajidinakaran1@gmail.com");
+                loginPage.SetPassword("Welcome123");
+                loginPage.ClickOnLogin();
+
+                //MainPage
+                MainPage mainPage = new MainPage(driver);
+                Thread.Sleep(5000);
+                String title= mainPage.getTitle();
+                Assert.AreEqual("My Account", title);
+                mainPage.ClickonLogOut();
+                driver.Quit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Message"+ ex);
+
+                Screenshot s = ((ITakesScreenshot)driver).GetScreenshot();
+                s.SaveAsFile("error.png");
+
+                Assert.Fail();
+            }
+           
+
+        }
+        [Test]
+        public void NegativeValidCredential()
+        {
+            IWebDriver driver = null;
+            try
+            {
+                driver = new ChromeDriver(@"D:\Mine\Company\Maveric\Driver");
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+                driver.Url = "https://magento.com/";
+
+                //HomePage
+                HomePage homePage = new HomePage(driver);
+                homePage.ClickOnMyAccount();
+
+                //LoginPage
+                LoginPage loginPage = new LoginPage(driver);
+                loginPage.SetUserName("balajidinak@gmail.com");
+                loginPage.SetPassword("Welcome123");
+                loginPage.ClickOnLogin();
+
+                //MainPage
+                MainPage mainPage = new MainPage(driver);
+                Thread.Sleep(5000);
+                String title = mainPage.getTitle();
+                Assert.AreEqual("My Account", title);
+                mainPage.ClickonLogOut();
+                driver.Quit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Message" + ex);
+
+                Screenshot s = ((ITakesScreenshot)driver).GetScreenshot();
+                s.SaveAsFile(@"D:\Mine\Company\UST-Global_Net\CSharp\MagentoAutomation\MagentoAutomation\error.png");
+
+                Assert.Fail();
+            }
+
+
+        }
+
+
+    }
+}
---
+++ MagentoAutomation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MagentoAutomation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MagentoAutomation")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("72954ea4-c5a0-463f-b7f9-ff90f1e21721")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
